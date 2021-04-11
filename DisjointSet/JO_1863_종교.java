526861d0afca51b9f97494a10c0dfbcc55a68197
Jihye Choi <77835382+hotpineapple@users.noreply.github.com>
2021-04-11 19:43:40 +0900

    rank 적용 수정

--- DisjointSet/JO_1863_종교.java
+++ DisjointSet/JO_1863_종교.java
@@ -3,26 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class JO_1863_종교 {
+public class JO_1863_종교_rank {
 
 	static int N;
 	static int[] parents;
+	static int[] rank;
 	static void make() {
 		for (int i = 1; i <= N; i++) {
 			parents[i] = -1;
 		}
 	}
 	static int findSet(int a) {
-		if(parents[a]<0) return a;
-		return parents[a] = findSet(parents[a]);
+		if(parents[a] < 0) return a;
+		return parents[a] = findSet(parents[a]); // path compression
 	}
 	static boolean union(int a, int b) {
 		int aRoot = findSet(a);
 		int bRoot = findSet(b);
 		if(aRoot == bRoot) return false;
 		
-		parents[aRoot] += parents[bRoot];
-		parents[bRoot] = aRoot;
+		// rank 비교
+		if(rank[aRoot] >= rank[bRoot]) {
+			parents[aRoot] += parents[bRoot];
+			parents[bRoot] = aRoot;
+			if(rank[aRoot] == rank[bRoot]) rank[aRoot]++;
+		}else {
+			parents[bRoot] += parents[aRoot];
+			parents[aRoot] = bRoot;
+		}
+		
 		return true;
 	}
 	public static void main(String[] args) throws IOException {
@@ -32,6 +41,7 @@ public class JO_1863_종교 {
 		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		parents = new int[N+1];
+		rank = new int[N+1];
 		make();
 		for(int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine()," ");
