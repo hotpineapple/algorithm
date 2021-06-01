5a66908ef7ebc1639181f44dbd3ee7aa9c470167
Jihye Choi <77835382+hotpineapple@users.noreply.github.com>
2021-06-01 16:36:47 +0900

    Create BOJ_11729_하노이탑이동.java

---
+++ Recursion/BOJ_11729_하노이탑이동.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static char map[][];
+	static int cnt;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		hanoi(1, 2, 3, N);
+		System.out.println(cnt);
+		System.out.println(sb.toString());
+		
+	}
+  
+	private static void hanoi(int start, int aux, int end, int n) {
+
+		cnt++;
+		if(n==1) {
+			sb.append(start + " " + end + "\n");
+			return;
+		}
+		
+		hanoi(start, end, aux, n-1);
+		sb.append(start + " " + end + "\n");
+		hanoi(aux, start, end, n-1);
+		
+	}
+
+}
