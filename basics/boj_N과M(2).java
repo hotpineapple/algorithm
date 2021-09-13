fc981f64b1564142aee1db306bf7bdd02af78c29
Jihye Choi <77835382+hotpineapple@users.noreply.github.com>
2021-09-13 19:18:23 +0900

    Create boj_N과M(2).java

---
+++ basics/boj_N과M(2).java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_nm2 {
+
+	static int N, M, output[];
+	static boolean[] isSelected;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		isSelected = new boolean[N];
+		output = new int[M];
+		
+		combination(0,1);
+
+	}
+
+	private static void combination(int cnt, int start) {
+		if(cnt==M){
+			print(output);
+			return;
+		}
+		
+		for(int i=start;i<=N;i++) {
+			output[cnt] = i;
+			combination(cnt+1,i+1);
+			
+		}
+	}
+
+	private static void print(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+}
