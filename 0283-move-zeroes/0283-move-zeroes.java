14d5253fe841121ddc2be19b2f4b3cb9db97b0eb
hotpineapple <77835382+hotpineapple@users.noreply.github.com>
2024-08-12 20:42:25 +0900

    Time: 1 ms (100.00%), Space: 45.8 MB (76.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int zeroCnt=0;
-       for (int i=0;i<nums.length;i++){
-            if(nums[i]==0) zeroCnt++;
-        }
         int j=0;
-        for(int i=0;i<nums.length;i++){
+        int len = nums.length;
+        for(int i=0;i<len;i++){
             if(nums[i]!=0) nums[j++]=nums[i];
         }
-        for(j=nums.length-zeroCnt;j<nums.length;j++){
-            nums[j]=0;
+        for(int i=j;i<len;i++){
+            nums[i]=0;
         }
-
     }
 }
