fc7f24cc660a29032a010ad6e19715ca4a40863a
hotpineapple <77835382+hotpineapple@users.noreply.github.com>
2024-10-20 14:33:23 +0900

    Time: 3 ms, Memory: 45.1 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.java
+++ valid-mountain-array/valid-mountain-array.java
@@ -2,23 +2,19 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
         if (arr.length<3) return false;
         
-        int[] cmp = new int[arr.length-1];
+        boolean flag= false;
+        int status = 0; // -1 for decreasing, 1 for increasing, 0 for no difference
         for (int i=0;i<arr.length-1;i++) {
             if (arr[i]==arr[i+1]) return false;
-            else if (arr[i]<arr[i+1]) { // strictly increase
-                cmp[i]=1;
-            }else {  // strictly decrease
-                cmp[i] = -1;
+            else if (arr[i]<arr[i+1]) {
+                if (status==-1) return false;
+                status = 1;
+            }else {
+                if(status==1) flag = true;
+                status = -1;
             }
         }
-        
-        int switchCnt=0;
-        for(int i=0;i<cmp.length-1;i++) {
-            if(cmp[i]==1 && cmp[i+1]==-1) switchCnt++;
-            else if (cmp[i]==-1 && cmp[i+1]==1) return false;
-        }
-        
-        if (switchCnt==1) return true;
+        if ( flag && status==-1) return true;
         return false;
     }
 }
